8e0e2a28f5601710270b7c65380d173825fc8a15
fmonroy75 <83427525+fmonroy75@users.noreply.github.com>
2024-09-05 19:26:17 -0400

    Update campana.py

--- campana.py
+++ campana.py
@@ -1,5 +1,5 @@
 class Campaña:
-
+    LARGO_NOMBRE_ANUNCIO=250
 
     def __init__(self, nombre: str, fecha_inicio: str, fecha_termino: str) -> None:
         self.__nombre = nombre
@@ -14,10 +14,11 @@ class Campaña:
 
     @nombre.setter
     def nombre(self, valor: str):
-        if len(valor) <= 250:
+        if len(valor) <= LARGO_NOMBRE_ANUNCIO:
             self.__nombre = valor
         else:
             raise LargoExcedidoException("El número de carácteres excede lo permitido")
+            
 
 
     @property
@@ -41,6 +42,27 @@ class Campaña:
         return self.__anuncios
 
 
+    def __str__(self) -> str:
+        n_videos = 0
+        n_display = 0
+        n_social = 0
+        
+        for anuncio in self.listado_anuncios:
+            if anuncio.__class__.__name__ == "Video":
+                n_videos += 1
+            elif anuncio.__class__.__name__ == "Display":
+                n_display += 1
+            else:
+                n_social += 1
+       
+        return f"""
+        Nombre de la campaña: {self.nombre}
+        Anuncios: {n_videos} Video, {n_display} Display, {n_social} Social
+        """
+    
+
     def crear_anuncio(self):
         tipo_anuncio = input("Ingrese el tipo de anuncio que desea crear ('Video', 'Display' o 'Social'):\n")
-    
+        while tipo_anuncio not in  ["V", "D", "S"]:
+            tipo_anuncio = input("Ingrese una de las opciones válidas('Video', 'Display' o 'Social'):\n")
+
