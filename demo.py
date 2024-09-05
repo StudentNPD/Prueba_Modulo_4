9cc3710d18ba719faa40a582ea1e91e914b24a1e
fmonroy75 <83427525+fmonroy75@users.noreply.github.com>
2024-09-05 19:47:42 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,7 +2,34 @@ from campaña import Campaña
 from error import Error, LargoExcedidoError, SubTipoInvalidoError
 from datetime import datetime
 
-
+NOMBRE_LOG="archivolog.log"
 
 #crear campaña
 nueva_campaña = Campaña("Nueva Campaña", "27/10/2024", "27/10/2024")
+
+
+log = None
+
+try:
+    #modificar campaña
+    nuevo_nombre = input("Ingrese el nuevo nombre de la campaña:\n")
+    nueva_campaña.nombre = nuevo_nombre
+
+except LargoExcedidoException as e:
+    fecha_actual = datetime.now()
+    with open(NOMBRE_LOG, 'a') as log:
+        log.write(f'{fecha_actual} : [ERROR]: {e}\n')
+
+except SubTipoInvalidoException as e:
+    fecha_actual = datetime.now()
+    with open(NOMBRE_LOG, 'a') as log:
+        log.write(f'{fecha_actual} - [ERROR]: {e}\n')
+
+except Error as e:
+    fecha_actual = datetime.now()
+    with open(NOMBRE_LOG, 'a') as log:
+        log.write(f'{fecha_actual} : [ERROR]: {e}\n')
+
+finally:
+    if log is not None:
+        log.close()
