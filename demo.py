615a07997e0dfe3b3ff283d548952bbe1499722e
fmonroy75 <83427525+fmonroy75@users.noreply.github.com>
2024-09-05 18:35:29 -0400

    Update demo.py

---
+++ demo.py
@@ -0,0 +1,8 @@
+from campaña import Campaña
+from error import Error, LargoExcedidoError, SubTipoInvalidoError
+from datetime import datetime
+
+
+
+#crear campaña
+nueva_campaña = Campaña("Nueva Campaña", "27/10/2024", "27/10/2024")
