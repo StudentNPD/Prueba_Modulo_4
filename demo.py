8d959cd160d9d214426d548903be8f698068987b
Natalia <90008571+StudentNPD@users.noreply.github.com>
2024-09-08 18:03:50 -0300

    demo.py ya almacena errores en un archivo. Falta por crear menu de acuerdo a lo pedido

--- demo.py
+++ demo.py
@@ -1,35 +1,49 @@
-from campaña import Campaña
-from error import Error, LargoExcedidoError, SubTipoInvalidoError
+from campana import Campana
+from error import Error, LargoExcedidoException, SubTipoInvalidoException
 from datetime import datetime
 
 NOMBRE_LOG="archivolog.log"
 
-#crear campaña
-nueva_campaña = Campaña("Nueva Campaña", "27/10/2024", "27/10/2024")
+
+#crear campana
+nueva_campana = Campana("Nueva Campana", "27/10/2024", "27/10/2024")
 
 
 log = None
+consultar = True
 
-try:
-    #modificar campaña
-    nuevo_nombre = input("Ingrese el nuevo nombre de la campaña:\n")
-    nueva_campaña.nombre = nuevo_nombre
+while consultar:
 
-except LargoExcedidoException as e:
-    fecha_actual = datetime.now()
-    with open(NOMBRE_LOG, 'a') as log:
-        log.write(f'{fecha_actual} : [ERROR]: {e}\n')
+    try:
+        #modificar campana
+        print("Creemos una campaña! :D")
+        
+        nuevo_nombre = input("Ingrese el nuevo nombre de la campana:\n")
+        print(nuevo_nombre)
+        nueva_campana.nombre = nuevo_nombre
+        nueva_campana.crear_anuncio()
+        consultar = False
+    except LargoExcedidoException as e:
+        fecha_actual = datetime.now()
+        # Imprime solo el mensaje
+        print(e)
+        with open(NOMBRE_LOG, 'a') as log:
+            log.write(f'{fecha_actual} : [ERROR]: {e}\n')
 
-except SubTipoInvalidoException as e:
-    fecha_actual = datetime.now()
-    with open(NOMBRE_LOG, 'a') as log:
-        log.write(f'{fecha_actual} - [ERROR]: {e}\n')
+    except SubTipoInvalidoException as e:
+        fecha_actual = datetime.now()
+        # Imprime solo el mensaje
+        print(e)
+        with open(NOMBRE_LOG, 'a') as log:
+            log.write(f'{fecha_actual} - [ERROR]: {e}\n')
 
-except Error as e:
-    fecha_actual = datetime.now()
-    with open(NOMBRE_LOG, 'a') as log:
-        log.write(f'{fecha_actual} : [ERROR]: {e}\n')
+    except Error as e:
+        fecha_actual = datetime.now()
+        # Imprime solo el mensaje
+        print(e)
+        with open(NOMBRE_LOG, 'a') as log:
+            log.write(f'{fecha_actual} : [ERROR]: {e}\n')
 
-finally:
-    if log is not None:
-        log.close()
+    finally:
+        if log is not None:
+            log.close()
