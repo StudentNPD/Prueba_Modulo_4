9a9b776c4ccc4223c909539b5c645053b1737b96
fmonroy75 <83427525+fmonroy75@users.noreply.github.com>
2024-09-08 19:52:48 -0400

    Update campana.py

--- campana.py
+++ campana.py
@@ -46,39 +46,21 @@ class Campana:
         return self.__anuncios
 
 
-    def __str__(self) -> str:
-        n_videos = 0
-        n_display = 0
-        n_social = 0
-        
-        for anuncio in self.listado_anuncios:
-            if anuncio.__class__.__name__ == "Video":
-                n_videos += 1
-            elif anuncio.__class__.__name__ == "Display":
-                n_display += 1
-            else:
-                n_social += 1
-        return f"""
-        Nombre de la campana: {self.nombre}
-        Anuncios: {n_videos} Video, {n_display} Display, {n_social} Social
-        """
-    
-
     def crear_anuncio(self):
-        tipo_anuncio = input("Ingrese el tipo de anuncio que desea crear ('Video', 'Display' o 'Social'):\n")
+        tipo_anuncio = input("Ingrese el tipo de anuncio que desea crear ('Video', 'Display' o 'Social'):\n").strip().upper
         while tipo_anuncio not in  ["V", "D", "S"]:
         #if tipo_anuncio not in  ["V", "D", "S"]: # No es necesario que sea un ciclo
             tipo_anuncio = input("Ingrese una de las opciones válidas('Video', 'Display' o 'Social'):\n")
-            #raise SubTipoInvalidoException("Ingrese en mayuscula solo la primera letra de la opción")
+         #   raise SubTipoInvalidoException("Ingrese en mayuscula solo la primera letra de la opción")
 
         url_archivo = input("Ingrese la url del archivo:\n")
         url_click = input("Ingrese la url del click:\n")
         sub_tipo = input("Ingrese el subtipo del anuncio:\n")
 
-        if tipo_anuncio == "Video":
+        if tipo_anuncio == "V":
             duracion = int(input("Ingrese la duración del video:\n"))
             nuevo_anuncio = Video(url_archivo, url_click, sub_tipo, duracion)
-        elif tipo_anuncio == "Display":
+        elif tipo_anuncio == "D":
             ancho = int(input("Ingrese el ancho del anuncio:\n"))
             alto = int(input("Ingrese el alto del archivo:\n"))
             nuevo_anuncio = Display(ancho, alto, url_archivo, url_click, sub_tipo)
@@ -90,7 +72,25 @@ class Campana:
         self.anuncios.append(nuevo_anuncio)
         return nuevo_anuncio
 
-    
+    def __str__(self) -> str:
+        num_videos = 0
+        num_display = 0
+        num_social = 0
+        
+        for anuncio in self.anuncios:
+            if anuncio.__class__.__name__ == "V":
+                num_videos += 1
+            elif anuncio.__class__.__name__ == "D":
+                num_display += 1
+            else:
+                num_social += 1
+        
+        
+        desc=f"Nombre de la campaña: {self.nombre}"
+        desc=desc + f"Anuncios: {num_videos} Video, {num_display} Display, {num_social} Social"
+                
+        return desc
+
     def __str__(self) -> str:
         num_videos = 0
         num_display = 0
